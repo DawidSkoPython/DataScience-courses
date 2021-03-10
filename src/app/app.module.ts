fb78fba26063fb0aa398b28e339415edd1ce1001
DawidSkoPython <dawid.skomorochow.python@gmail.com>
2021-03-10 19:52:51 +0100

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { HomeComponent } from './home/pages/home/home.component';
+import { CategoriesComponent } from './home/pages/home/categories/categories.component';
+import { ArticlesComponent } from './home/pages/home/articles/articles.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,20 +9,28 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
 import { FooterComponent } from './shared/layout/footer/footer.component';
 
-
+import {HttpClientModule} from '@angular/common/http'
+import {HttpClient} from '@angular/common/http';
+import { Article1Component } from './home/pages/home/article1/article1.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-
+    ArticlesComponent,
+    CategoriesComponent,
+    Article1Component,
+    HomeComponent,
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+ 
+    
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
